bd24f347b88021e8737dd6eae5665d0747b33ea0
William Oliveira <sudowilliam@gmail.com>
2020-05-24 23:52:12 -0300

    Update shaders location

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@
 const gl = canvas.getContext('webgl');
 
 // Vertex shader
-const vshader = import('./shaders/simple_vshader.glsl');
+const vshader = `
+void main() {
+  // set vertex position: vec4(x, y, z, 1.0)
+  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+
+  // point size in pixels: float
+  gl_PointSize = 10.0;
+}
+`;
 
 // Fragment shader
-const fshader = import('./shaders/simple_fshader.glsl');
+const fshader = `
+precision mediump float;
+
+void main() {
+  // set fragment color: vec4(r, g, b, alpha)
+  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+`;
 
 const program = compile(gl, vshader, fshader);
 
--- src/shaders/simple_vshader.glsl
+++
@@ -1,7 +0,0 @@
-void main() {
-  // set vertex position: vec4(x, y, z, 1.0)
-  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-
-  // point size in pixels: float
-  gl_PointSize = 10.0;
-}
