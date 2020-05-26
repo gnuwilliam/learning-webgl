17b660ab5caee61f281b91e675d36c4d09657ec3
William Oliveira <sudowilliam@gmail.com>
2020-05-26 01:33:59 -0300

    New presentation shader

---
+++ src/draw_squares.js
@@ -0,0 +1,55 @@
+/** @type {WebGLRenderingContext} */
+const gl = canvas.getContext('webgl', { preserveDrawingBuffer: true });
+
+// Vertex shader
+const vshader = `
+// make attributes accesible externally
+attribute vec4 position;
+attribute float size;
+void main() {
+  // set vertex position: vec4(x, y, z, 1.0)
+  gl_Position = position;
+
+  // point size in pixels: float
+  gl_PointSize = size;
+}
+`;
+
+// Fragment shader
+const fshader = `
+precision mediump float;
+uniform vec4 color;
+
+void main() {
+  // set fragment color: vec4(r, g, b, alpha)
+  gl_FragColor = color;
+}
+`;
+
+const program = compile(gl, vshader, fshader);
+
+// get position, size and color of the shader
+const position = gl.getAttribLocation(program, 'position');
+const size = gl.getAttribLocation(program, 'size');
+const color = gl.getUniformLocation(program, 'color');
+
+// Set the clear color (black)
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+// Clear the canvas
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+setInterval(() => {
+  const x = Math.random() * 2 - 1;
+  const y = Math.random() * 2 - 1;
+
+  const r = Math.random();
+  const g = Math.random();
+  const b = Math.random();
+
+  gl.vertexAttrib3f(position, x, y, 0);
+  gl.vertexAttrib1f(size, 10);
+  gl.uniform4f(color, r, g, b, 1);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
+}, 500);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ const gl = canvas.getContext('webgl', { preserveDrawingBuffer: true });
 
 // Vertex shader
 const vshader = `
-// make attributes accesible externally
 attribute vec4 position;
-attribute float size;
 void main() {
-  // set vertex position: vec4(x, y, z, 1.0)
   gl_Position = position;
-
-  // point size in pixels: float
-  gl_PointSize = size;
 }
 `;
 
@@ -19,9 +13,7 @@ void main() {
 const fshader = `
 precision mediump float;
 uniform vec4 color;
-
 void main() {
-  // set fragment color: vec4(r, g, b, alpha)
   gl_FragColor = color;
 }
 `;
@@ -30,26 +22,22 @@ const program = compile(gl, vshader, fshader);
 
 // get position, size and color of the shader
 const position = gl.getAttribLocation(program, 'position');
-const size = gl.getAttribLocation(program, 'size');
 const color = gl.getUniformLocation(program, 'color');
+gl.uniform4f(color, 1, 0, 0, 1);
+
+// prettier-ignore
+const vertices = new Float32Array([
+  0, 0.5, 0, // point 1
+  -0.5, -0.5, 0, // point 2
+  0.5, -0.5, 0 // point 3
+]);
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
 // Set the clear color (black)
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
 // Clear the canvas
 gl.clear(gl.COLOR_BUFFER_BIT);
-
-setInterval(() => {
-  const x = Math.random() * 2 - 1;
-  const y = Math.random() * 2 - 1;
-
-  const r = Math.random();
-  const g = Math.random();
-  const b = Math.random();
-
-  gl.vertexAttrib3f(position, x, y, 0);
-  gl.vertexAttrib1f(size, 10);
-  gl.uniform4f(color, r, g, b, 1);
-
-  gl.drawArrays(gl.POINTS, 0, 1);
-}, 500);
